e52ef3c07e14fef761e1625a2ffb2a173dc852cb
Shivashish Bhaskar <shivashishbhaskar@gmail.com>
2022-08-19 22:48:03 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const mongoose = require('mongoose')
+const app = express()
+app.use(bodyparser.json())
+
+const port = 6000
+const mongoURI = "mongodb://127.0.0.1:27017/mytestdb";
+
+mongoose.connect(mongoURI)
+.then(res => console.log('db connection success'))
+.catch(err => console.log('error'))
+
+const user = {
+    title:{type:String},
+    id:{type:String},
+    description:{type:String}
+}
+
+const Blog = mongoose.model('users',user)
+
+app.get('/',(req,res) => {
+     res.send('hello world');
+})
+
+app.post('/contact',(req,res) => {
+    console.log(req.body);
+    const blog1 = new Blog ({
+        title:req.body.title,
+        id:req.body.id,
+        description:req.body.description
+    })
+    blog1.save((err,result) =>{
+        if(err){console.log('error')}
+        else{'data added'}
+    })
+})
+
+app.get('/fetch',(req,res)=>{
+    Blog.find({},(err,docs)=>{
+        if(err){
+            console.llog('err')
+        }else{
+            res.send(docs)
+        }
+    })
+})
+
+app.listen(6000,() => console.log("server started"))
